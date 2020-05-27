2fe547d2118ce3425076e2b6f353ea03403e13aa
Rony Costa <ronybroro023@gmail.com>
2020-05-27 17:41:28 -0300

    Lêr o dia, mês e ano de nascimento de um usuário e exibir a data formatada

---
+++ MUNDO01/AULAS/AULA04/DESAFIOS/desafio02.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia o dia, mês e ano de nascimento de uma pessoa
+# e escreva uma mensagem com a data formatada
+dia = input('Qual o dia de nascimento? ')
+mes = input('Qual o mês de nascimento? ')
+ano = input('Qual o ano de nascimento? ')
+print('Você nasceu no dia,', dia, 'de', mes, 'de', ano,'. Correto?')
