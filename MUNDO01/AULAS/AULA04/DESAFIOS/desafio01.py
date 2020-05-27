3b2af2e16054b74a9060ed12e15da3fee1756cd5
Rony Costa <ronybroro023@gmail.com>
2020-05-27 17:29:39 -0300

    Mostrar Mensagem de boas-vindas na tela

---
+++ MUNDO01/AULAS/AULA04/DESAFIOS/desafio01.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem
+# de boas-vindas de acordo com o valor digitado.
+
+# Solicitando que o usuário informe seu nome.
+nome = input('Qual o seu nome? ')
+# Exibindo mensagem de boas-vindas na tela.
+print('Seja bem vindo ao Curso em Vídeo de Python 3', nome)
+print('É um prazer te conhecer!')
