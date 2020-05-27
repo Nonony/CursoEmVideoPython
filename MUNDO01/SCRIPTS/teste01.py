89c6590fa38dfecdc9e6e30fc3a79f6e18e89af9
Rony Costa <ronybroro023@gmail.com>
2020-05-27 17:19:56 -0300

    Utilização de variáveis com função input() e saida de dados usando função print()

---
+++ MUNDO01/SCRIPTS/teste01.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que digite seu nome.
+nome = input('Qual o seu nome? ')
+# Solicita ao usuário que digite sua idade.
+idade = input('Quantos anos você tem? ')
+# Solicita ao usuário que informe seu peso.
+peso = input('Qual o seu peso? ')
+# Escreve na tela o nome, a idade e o peso do usuário
+print(nome, idade, peso)
