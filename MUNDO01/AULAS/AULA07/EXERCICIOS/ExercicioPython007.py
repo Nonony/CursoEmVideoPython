b92743524dec2015e543bea807778485984f15a6
Rony Costa <ronybroro023@gmail.com>
2020-05-28 17:36:22 -0300

    Média aritmética

---
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython007.py
@@ -0,0 +1,6 @@
+# Escreva um program que leia duas notas de um aluno, calcule e mostre sua média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+print('Caro aluno, suas notas foram respectivamente: {} e {}\nSua média aritimética é: {}'.format(n1, n2, media))
