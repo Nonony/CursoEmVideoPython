efdee8e632146cd9fb6aefc82cbfb7536ad51a01
Rony Costa <ronybroro023@gmail.com>
2020-05-27 22:27:18 -0300

    Utilizando a função print() em conjunto com .format() para saídas formatadas.

---
+++ MUNDO01/AULAS/AULA04/EXERCICIOS/ExercicioPython002.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas na tela.
+nome = input('Qual o seu nome? ')
+# Utilizaremos a função .format() em conjunto com a função print()
+print('Olá {}.\nÉ um prazer te conhecer!'.format(nome))
