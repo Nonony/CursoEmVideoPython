fc64201a9a0d1631ef61dafd47e9ee5965e29a4d
Rony Costa <ronybroro023@gmail.com>
2020-05-27 23:14:24 -0300

    Utilização do tipo primitivo INT para realização de calculos matemáticos

---
+++ MUNDO01/SCRIPTS/testeAula06A.py
@@ -0,0 +1,9 @@
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+soma = num1 + num2
+# Verificar o tipo primitivo de num1
+print(type(num1))
+# Verificar o tipo primitivo de num2
+print(type(num2))
+# Escrever na tela o resultado da soma entre num1 e num2
+print('A soma entre {} e {} é {}.'.format(num1, num2, soma))
