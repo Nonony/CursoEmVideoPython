222b08c747373fc218f514c0b8ed7820884ab6b1
Rony Costa <ronybroro023@gmail.com>
2020-05-28 13:44:01 -0300

    Somar números utilizando tipo primitivo INT e saida formatada com .format()

---
+++ MUNDO01/AULAS/AULA06/EXERCICIOS/ExercicioPython003.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo nome: '))
+s = n1 + n2
+print('O resultado da soma entre {} e {} é: {}'.format(n1, n2, s))
