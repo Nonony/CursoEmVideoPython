0942093740ad3a5f67598aa67044d6cea2b33567
Rony Costa <ronybroro023@gmail.com>
2020-05-27 22:03:35 -0300

    Soma entre dois números. ERRO devido a não utilização de tipo primitivo

---
+++ MUNDO01/AULAS/AULA04/DESAFIOS/desafio03.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia dois números e tente mostrar a soma entre eles
+# Versão do programa sem utiliza o tipo primitivo INT
+num1 = input('Primeiro número: ')
+num2 = input('Segundo número: ')
+soma = num1 + num2
+print('A soma entre os números', num1, 'e', num2, 'é: ', soma)
