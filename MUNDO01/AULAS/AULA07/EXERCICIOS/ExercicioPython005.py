3225a4b9eeb44b5acd461bedca551dfda74b48fb
Rony Costa <ronybroro023@gmail.com>
2020-05-28 17:34:51 -0300

    Sucessor e Antecessor de um número inteiro.

---
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython005.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+
+n = int(input('Digite um número inteiro: '))
+print('Antecessor: {} - Número digitado: {} - Sucessor: {}'.format(n - 1, n, n + 1))
