50466e9cf527006f511e6c1c2a19e90c77428abd
Rony Costa <ronybroro023@gmail.com>
2020-05-29 14:50:44 -0300

    Correção ortográfica

--- MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython008.py
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython008.py
@@ -1,6 +1,6 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 
-metros = float(input('Informe a medida que deseja converter mara cm e mm: '))
+metros = float(input('Distância em metros(m): '))
 cm = metros * 100
 mm = metros * 1000
 print('{}m equivalem a {}cm e {}mm'.format(metros, cm, mm))
