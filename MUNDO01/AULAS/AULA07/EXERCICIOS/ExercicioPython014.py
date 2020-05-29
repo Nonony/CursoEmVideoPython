3e0ac1471d19e38791ea6d5d08742eb0004edc5e
Rony Costa <ronybroro023@gmail.com>
2020-05-29 15:45:58 -0300

    Correção ortográfica

--- MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython014.py
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython014.py
@@ -1,6 +1,6 @@
 # Escreva um programa que leia uma temperatura em Graus Centigrados e converta para Fahrenheit
 # Formula de conversão: (0 °C × 9/5) + 32 = 32 °F
 
-temperatura = float(input('Qual temperatura deseja converter para Fahrenheit? '))
+temperatura = float(input('Temperatura em °C: '))
 conversao = (temperatura * (9 / 5) + 32)
-print('A temperatura {}°C equivalem a {}°F'.format(temperatura, conversao))
+print('A temperatura {}°C equivale a {}°F'.format(temperatura, conversao))
