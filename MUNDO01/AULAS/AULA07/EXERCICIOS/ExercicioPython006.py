9bd303ae2d4f45c89948fce91389725372053bc4
Rony Costa <ronybroro023@gmail.com>
2020-05-29 14:33:51 -0300

    Adicionei formulas para calculo de raiz quadrada

--- MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython006.py
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython006.py
@@ -1,9 +1,13 @@
 # Crie um programa que leia um número
 # Mostre seu dobro, triplo e raiz quadrada
+"""
+A raiz quadrada pode ser descoberta usando elevando o número desejado a (1/2)
+A raiz quadrada também pode ser obtida utilizando a função pow(n, (1/2))
+"""
 
 n = int(input('Digite um número: '))
 print('O número digitado foi o {}\n'
       'Seu dobro é {}\n'
       'Seu triplo é {}\n'
       'Ele tem raiz quadrada igual a {:.2f}'.format(n, n * 2, n * 3, n ** (1/2)))
-# A raiz quadrada também pode ser obtida utilizando a função pow(n, (1/2))
+
