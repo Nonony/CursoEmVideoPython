65a4e108a594236bb4c02df12c19321177660165
Rony Costa <31996129+Nonony@users.noreply.github.com>
2020-05-31 20:56:10 -0300

    Coreção da formatação da saída do print

--- MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython014.py
+++ MUNDO01/AULAS/AULA07/EXERCICIOS/ExercicioPython014.py
@@ -3,4 +3,4 @@
 
 temperatura = float(input('Temperatura em °C: '))
 conversao = (temperatura * (9 / 5) + 32)
-print('A temperatura {}°C equivale a {}°F'.format(temperatura, conversao))
+print('A temperatura {}°C equivale a {:.1f}°F'.format(temperatura, conversao))
