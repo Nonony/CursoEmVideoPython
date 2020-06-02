c99f17f859bbaebfae99473922fca3865d2c81cd
Rony Costa <ronybroro023@gmail.com>
2020-06-01 21:41:19 -0300

    Exercício 16 – Quebrando um número.

---
+++ MUNDO01/AULAS/AULA08/EXERCICIOS/ExercicioPython016.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um número real qualquer pelo teclado e mostre na tela sua porção inteira.
+# Ex: Digite um Número: 6.127
+# O número 6.127 tem a parte inteira 6.
+
+from math import trunc
+num = float(input('Digite um número: '))
+print('O número {} tem a parte inteira {}.'.format(num, trunc(num)))
