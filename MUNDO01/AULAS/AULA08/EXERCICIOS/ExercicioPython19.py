41574e5d526ab0145a34fa7b61d677204d57a4ba
Rony Costa <ronybroro023@gmail.com>
2020-06-27 23:13:06 -0300

    Correção: Escolhendo um nome aleatória de uma lista.

--- MUNDO01/AULAS/AULA08/EXERCICIOS/ExercicioPython19.py
+++ MUNDO01/AULAS/AULA08/EXERCICIOS/ExercicioPython19.py
@@ -1,7 +1,13 @@
-# Escreva um programa que Leia o nome de quatro pessoas e escolha aleatoriamente um deles e escreva
-# seu nome na tela
+# Escreva um programa que Leia o nome de quatro pessoas e escolha
+# aleatoriamente um deles e escreva seu nome na tela
 
 import random
-nome = ['Carlos', 'Felipe', 'Marcos', 'Rogério', 'Rony']
-escolha = random.choice(nome)
+
+nome1 = str(input('Informe o primeiro nome: '))
+nome2 = str(input('Informe o segundo nome: '))
+nome3 = str(input('Informe o terceiro nome: '))
+nome4 = str(input('Informe o quarto nome: '))
+lista = [nome1, nome2, nome3, nome4]
+print(lista)
+escolha = random.choice(lista)
 print('Parabéns {}, você é o escolhido!'.format(escolha))
