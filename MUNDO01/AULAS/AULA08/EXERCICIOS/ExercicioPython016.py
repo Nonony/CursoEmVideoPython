da61a70c8e787368a02965e9aed973823d116c42
Rony Costa <ronybroro023@gmail.com>
2020-06-27 23:10:21 -0300

    Exibir apenas a parte inteira de um número

--- MUNDO01/AULAS/AULA08/EXERCICIOS/ExercicioPython016.py
+++ MUNDO01/AULAS/AULA08/EXERCICIOS/ExercicioPython016.py
@@ -5,3 +5,5 @@
 from math import trunc
 num = float(input('Digite um número: '))
 print('O número {} tem a parte inteira {}.'.format(num, trunc(num)))
+# Obtendo a parte inteira de um número sem utilizar o módulo math
+print('O número {} tem a parte inteira {}.'.format(num, int(num)))
