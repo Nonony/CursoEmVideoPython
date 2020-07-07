bd2f000be1402fcf959c2a581673b0650f50890b
Rony Costa <ronybroro023@gmail.com>
2020-07-07 08:54:07 -0300

    exercício 22 correção

--- MUNDO01/AULAS/AULA09/EXERCICIOS/ExercicioPython022.py
+++ MUNDO01/AULAS/AULA09/EXERCICIOS/ExercicioPython022.py
@@ -4,11 +4,13 @@ O nome com todas as letras minúsculas
 Total de caracteres da frase (sem considerar espaços)
 Quantas letras tem o primeiro nome.
 """
-
+import time
 nome = str(input('Digite seu nome completo: '))
-print(nome.upper())
-print(nome.lower())
-print('O total de letras no nome é: {}'.format(len(nome.replace(' ', ''))))
+print('Analisando o nome informado...')
+time.sleep(2)
+print('Seu nome em letras maiúsculas fica assim: {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas fica assim: {}'.format(nome.lower()))
+print('O total de letras no nome em contar espaços é: {}'.format(len(nome.replace(' ', ''))))
 div = nome.split()
 # print(div[1])
 print('O primeiro nome tem um total de {} letras'. format(len(div[0])))
