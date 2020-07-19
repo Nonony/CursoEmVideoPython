74690a24e99ae257c9f687306869d7c7515e2d97
Rony Costa <ronybroro023@gmail.com>
2020-07-18 23:05:33 -0300

    Exercicio 34 Calculando Salários

--- MUNDO01/AULAS/AULA10/EXERCICIOS/ExercicioPython34.py
+++ MUNDO01/AULAS/AULA10/EXERCICIOS/ExercicioPython34.py
@@ -1,6 +1,6 @@
 """Exercício Python 34:
 Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
-Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%."""
+Para salários superiores  a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%."""
 
 sal = float(input('Qual o valor do salário: '))
 calc1 = 0.1
