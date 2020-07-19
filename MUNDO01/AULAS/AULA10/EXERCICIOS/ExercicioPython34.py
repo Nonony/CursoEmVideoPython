aaffb4f9457d96696914a25b55b62c3ff9ec3d4d
Rony Costa <ronybroro023@gmail.com>
2020-07-18 22:29:43 -0300

    exercício 33 Maior e menor valores

---
+++ MUNDO01/AULAS/AULA10/EXERCICIOS/ExercicioPython34.py
@@ -0,0 +1,13 @@
+"""Exercício Python 34:
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%."""
+
+sal = float(input('Qual o valor do salário: '))
+calc1 = 0.1
+calc2 = 0.15
+val = ''
+if sal <= 1250.00:
+    val = (sal * calc1) + sal
+if sal > 1250.00:
+    val = (sal * calc2) + sal
+print('O novo valor do salário é R${:.2f}'.format(val))
