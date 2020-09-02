50871da986961c1050c6560360ef4c93a07bd409
Rony Costa <31996129+Nonony@users.noreply.github.com>
2020-09-01 21:54:45 -0300

    Pedra, papel, tesoura.

--- MUNDO02/AULAS/AULA13/EXERCICIOS/ExercicioPython46.py
+++ MUNDO02/AULAS/AULA13/EXERCICIOS/ExercicioPython46.py
@@ -1,15 +1,6 @@
 """Exercício Python 46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de
 artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles."""
 
-<<<<<<< HEAD
-import time as tm
-print('Contagem regressiva para os fogos de artificio!')
-for c in range(10, -1, -1):
-    tm.sleep(1)
-    print(c)
-tm.sleep(1)
-print('KABUM!... BADUMM...! POW...!')
-=======
 import time
 print('Contagem regressiva:')
 for c in range(10, -1, -1):
@@ -17,4 +8,3 @@ for c in range(10, -1, -1):
     print(c)
 time.sleep(1)
 print('Kaboom, Bumm, Pow...!')
->>>>>>> origin/master
